dfafa7e9921e4826e7cc651f59a5b329328b8383
Saksham Sharma <145899772+sakksham27@users.noreply.github.com>
2025-07-02 22:14:52 -0400

    Create bronze_scheme_exploration.sql

---
+++ data_quality_checks/bronze_scheme_exploration.sql
@@ -0,0 +1,21 @@
+-- Row count: athlete biography
+SELECT COUNT(*) FROM bronze.olympic_athlete_biography;
+
+-- Row count: event details
+SELECT COUNT(*) FROM bronze.olympic_athlete_event_details;
+
+-- Row count: country profiles
+SELECT COUNT(*) FROM bronze.olympic_country_profiles;
+
+-- Row count: event results
+SELECT COUNT(*) FROM bronze.olympic_event_results;
+
+-- Row count: games summary
+SELECT COUNT(*) FROM bronze.olympic_games_summary;
+
+-- Row count: medal tally history
+SELECT COUNT(*) FROM bronze.olympic_medal_tally_history;
+
+/*
+Note: This file is used as a check to ensure full data has been loaded
+*/
